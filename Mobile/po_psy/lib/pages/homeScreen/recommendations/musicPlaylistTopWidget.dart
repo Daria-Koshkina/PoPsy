7df3207975d4a2f7228ea8128f025c826e61523f
Dasha Koshkina <koshkinadasha2002@gmail.com>
2021-05-07 14:55:46 +0300

    Added top bar for music playlist page

---
+++ Mobile/po_psy/lib/pages/homeScreen/recommendations/musicPlaylistTopWidget.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:po_psy/assets/my_icons_icons.dart';
+import 'package:po_psy/constants/UIConstants/ColorPallet.dart';
+import 'package:po_psy/constants/UIConstants/TextStyles.dart';
+import 'package:po_psy/models/MusicPlaylist.dart';
+
+class MusicPlaylistTopWidget extends StatelessWidget {
+  final MusicPlaylist playlist;
+  final VoidCallback play;
+  final VoidCallback mix;
+
+  MusicPlaylistTopWidget({this.playlist, this.play, this.mix});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 300,
+      color: ColorPallet.redBlockColor,
+      child: Column(
+        children: [
+          SizedBox(height: 35,),
+          Padding(
+              padding: new EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.arrow_back_ios, color: Colors.white,),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                  Spacer(),
+                  IconButton(icon: Icon(MyIcons.exit_up, color: Colors.white,),
+                      onPressed: () {}),
+                ],
+              )
+          ),
+          SizedBox(height: 20,),
+          Padding(padding: new EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              children: [
+                Container(
+                    width: 120,
+                    height: 120,
+                    child: Image.network(playlist.imageURL)),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    child: Column(
+                        children: [
+                          Text(playlist.title,
+                            style: TextStyles.lightHeaderTextStyle,),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                  height: 36,
+                                  width: 92,
+                                  child: TextButton.icon(
+                                    label: Text(
+                                      'Play',
+                                      style: TextStyles.songTitleTextStyle,),
+                                    icon: Icon(
+                                      Icons.play_arrow_rounded,
+                                      color: ColorPallet.subsidiaryTextColor,
+                                    ),
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: ColorPallet
+                                          .lightGreyColor,
+                                    ),
+                                    onPressed: () => play(),
+                                  )
+                              ),
+                              Spacer(),
+                              SizedBox(
+                                  height: 36,
+                                  width: 92,
+                                  child: TextButton.icon(
+                                    label: Text(
+                                      'Mix',
+                                      style: TextStyles.songTitleTextStyle,),
+                                    icon: Icon(
+                                      Icons.autorenew,
+                                      color: ColorPallet.subsidiaryTextColor,
+                                    ),
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: ColorPallet
+                                          .lightGreyColor,
+                                    ),
+                                    onPressed: () => mix(),
+                                  )
+                              ),
+                            ],
+                          )
+                        ]
+                    )
+                )
+              ],
+            ),)
+        ],
+      ),
+    );
+  }
+}
