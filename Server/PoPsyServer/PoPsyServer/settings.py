220eeae059d583fcee77bcd695e6c0f3a59f2223
Yurambolo <61801166+Yurambolo@users.noreply.github.com>
2021-05-08 01:16:07 +0300

    DB v1.0
    DB on mysql added + admin page

--- Server/PoPsyServer/PoPsyServer/settings.py
+++ Server/PoPsyServer/PoPsyServer/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'PoPsyServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'popsy',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
     }
 }
 
