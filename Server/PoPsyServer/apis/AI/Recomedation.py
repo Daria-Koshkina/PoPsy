f16fc4895d2713f6fa1a5fc206f0e09dd7ea19ea
avojarot <64825394+avojarot@users.noreply.github.com>
2021-05-12 11:39:23 +0300

    Changes in structure + imports for recomendations

---
+++ Server/PoPsyServer/apis/AI/Recomedation.py
@@ -0,0 +1,3 @@
+from .TextAI import TextAI
+from apis.models import User, UserEmotions, ArticleView, VideoListView, AudioListView, Article, AudioList, VideoList
+
--- Server/PoPsyServer/apis/AI/TextAI.py
+++ Server/PoPsyServer/apis/AI/TextAI.py
@@ -1,36 +1,39 @@
 import pickle
-from tensorflow.keras.layers import Flatten, Dense, Input
+from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.optimizers import Adam
 from googletrans import Translator
 import re
 
-def load_nn():
-    input_layer = Input(shape=(6631))
-    x = Dense(units=100, activation="relu")(input_layer)
-    x = Dropout(rate=0.7)(x)
 
-    output_layer = Dense(units=13, activation="softmax")(x)
+class TextAI:
 
-    model = Model(input_layer, output_layer)
-    opt = Adam()
-    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
-    model.load_weights("model1.h5")
-    return model
+    def __init__(self):
+        self.model = self.load_nn()
+        self.translator = Translator()
+        self.tokenizer = pickle.load(open("tokenizer.picle", 'rb'))
 
-model = load_nn()
-translator = Translator()
-tokenizer = pickle.load(open("tokenizer.picle", 'rb'))
+    def load_nn(self):
+        input_layer = Input(shape=(6631))
+        x = Dense(units=100, activation="relu")(input_layer)
+        x = Dropout(rate=0.7)(x)
 
-def textPreprosses(text):
-    temp = Translator.translate(text).text
-    temp = re.sub(r"(@.*?)(\s|$)", "", temp)
-    temp = re.sub(r"(http:\/\/.*?)(\s|$)", "", temp)
-    temp.replace("#", "")
-    return tokenizer.transform(temp).toarray()
+        output_layer = Dense(units=13, activation="softmax")(x)
 
-def predictEmotion(text):
-    value = textPreprosses(text);
-    return model.predict(value);
+        model = Model(input_layer, output_layer)
+        opt = Adam()
+        model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+        model.load_weights("model1.h5")
+        return model
+
+    def text_preprosses(self, text):
+        temp = Translator.translate(text).text
+        temp = re.sub(r"(@.*?)(\s|$)", "", temp)
+        temp = re.sub(r"(http:\/\/.*?)(\s|$)", "", temp)
+        temp.replace("#", "")
+        return self.tokenizer.transform(temp).toarray()
+
+    def predictEmotion(self, text):
+        value = self.text_preprosses(text);
+        return self.model.predict(value);
