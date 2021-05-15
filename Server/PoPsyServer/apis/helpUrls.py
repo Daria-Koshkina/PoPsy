a2fbacafbe13e4f2e157e1674417ef027360be09
avojarot <64825394+avojarot@users.noreply.github.com>
2021-05-15 18:57:07 +0300

    fix

--- Server/PoPsyServer/apis/helpUrls.py
+++ Server/PoPsyServer/apis/helpUrls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from apis.views import apiViews as views
 
 urlpatterns = [
-    path('getMusicPlayLists/', views.getMusicPlayLists),
     path('getContent/', views.getContent),
     path('register/', views.register),
     path('signIn/', views.singIn)
