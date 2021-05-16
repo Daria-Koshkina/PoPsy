4aaf9a590f16bc41e893faa6c03c2c6d6b5a2a70
NChervinska <anastasiia.chervinska@nure.ua>
2021-05-16 19:12:33 +0300

    commit

--- Mobile/po_psy/lib/main.dart
+++ Mobile/po_psy/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: ColorPallet.mainColor,
           ),
-          home: EditorPage(),
+          home: HomePage(),
         ));
   }
 }
