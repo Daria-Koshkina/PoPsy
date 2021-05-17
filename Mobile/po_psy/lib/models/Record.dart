a97fbe9ec3749be8f37fbd92575281cfdb79c5fb
avojarot <64825394+avojarot@users.noreply.github.com>
2021-05-17 18:18:52 +0300

    Tests api on user

--- Mobile/po_psy/lib/models/Record.dart
+++ Mobile/po_psy/lib/models/Record.dart
@@ -4,4 +4,12 @@ class Record {
   //final File content;
 
   Record(this.Date, this.type,/* this.content, */);
+
+  factory Record.fromJson(Map<String, dynamic> json){
+    return Record(
+      json['Date'],
+      json['type']
+      //json['content']
+    );
+  }
 }
